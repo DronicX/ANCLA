f1f0adf717b626df700df8d0c5916bd75be81d25
DronicX <madeforspams@gmail.com>
2018-10-15 16:43:22 -0400

    Updated to print if verbose is true and write file if datalog is true

--- analysis/sentimentAnalysis.py
+++ analysis/sentimentAnalysis.py
@@ -3,6 +3,8 @@ from time import sleep
 from datetime import datetime
 from textblob import TextBlob #For Sentiment Analysis
 import matplotlib.pyplot as plt #For Graphing the Data
+from settingHandler import loadSettings
+from writeFile import writeToFile
 
 CONSUMER_KEY = '5bKnzZoSPFqdyxxqaQ5K3UswP'
 CONSUMER_SECRET = 'L5jQ1a0Ya25aW6GRcMMrHpvrTSoRJGLp7vmA7nC9NeBVpaxIXX'
@@ -13,24 +15,51 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-#Print your user name
-#user = api.me()
-#print (user.name)
-
 def runSentimentAnalysis(keyword, tweetNumber = 100):
     tweetNumber = int(tweetNumber)
 
     polarity_list = []
     numbers_list = []
     number = 1
+    settings = loadSettings()
+    tweetJSON = {}
+    tweetJSON['Tweets'] = []
 
     for tweet in tweepy.Cursor(api.search, keyword, lang="en").items(tweetNumber):
         try:
+
             analysis = TextBlob(tweet.text)
             analysis = analysis.sentiment
             polarity = analysis.polarity
             polarity_list.append(polarity)
             numbers_list.append(number)
+
+            if settings["verbose"] == True:
+                #Current count        
+                print("Tweet: " + str(number))
+                #Tweet text
+                print(tweet.text)
+                #Sentiment value
+                print("Sentiment value: " + str(polarity) + "\n")
+
+            if settings["datalog"] == True:
+                tweetJSON['Tweets'].append({
+                    'tweet_id': tweet.id,
+                    'tweet_created': str(tweet.created_at),
+                    'text': tweet.text,
+                    'sentiment': polarity,
+                    'favorites': tweet.favorite_count,
+                    'retweets': tweet.retweet_count,
+                    'user_name': tweet.user.name,
+                    'user_handle': tweet.user.screen_name,
+                    'verified': tweet.user.verified,
+                    'followers': tweet.user.followers_count,
+                    'friends': tweet.user.friends_count,
+                    'user_likes': tweet.user.favourites_count,
+                    'user_tweets': tweet.user.statuses_count,
+                    'user_created': str(tweet.user.created_at)    
+                })
+
             number = number + 1
 
         except tweepy.TweepError as e:
@@ -39,7 +68,11 @@ def runSentimentAnalysis(keyword, tweetNumber = 100):
         except StopIteration:
             break
 
+    writeToFile(tweetJSON, 'analyze-sentiment')
+       
     #Here we define axes
+    plt.figure(num=1)
+    plt.ion()
     axes = plt.gca()
     axes.set_ylim([-1, 2])
 
@@ -49,10 +82,12 @@ def runSentimentAnalysis(keyword, tweetNumber = 100):
     averagePolarity = (sum(polarity_list))/(len(polarity_list))
     averagePolarity = "{0:.0f}%".format(averagePolarity * 100)
     time  = datetime.now().strftime("At: %H:%M\nOn: %m-%d-%y")
-    plt.text(0, 1.25, "Average Sentiment:  " + str(averagePolarity) + "\n" + time, fontsize=12, bbox = dict(facecolor='none', edgecolor='black', boxstyle='square, pad = 1'))
+    plt.text(1, 1.25, "Average Sentiment:  " + str(averagePolarity) + "\n" + time, fontsize=12, bbox = dict(facecolor='none', edgecolor='black', boxstyle='square, pad = 1'))
 
     plt.title("Sentiment of " + keyword + " on Twitter") 
     plt.xlabel("Number of Tweets")
     plt.ylabel("Sentiment")
 
-    plt.show()
+    while plt.fignum_exists(1):
+        plt.show()
+        plt.pause(0.0001)
