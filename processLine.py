a64917b9f7ce3a9167b61593ac5fd404c0b0b0ba
DronicX <madeforspams@gmail.com>
2018-10-18 09:43:09 -0400

    Code cleanup

--- processLine.py
+++ processLine.py
@@ -1,7 +1,6 @@
 import pickle
 from analysis.sentimentAnalysis import runSentimentAnalysis
 from analysis.liveSentimentAnalysis import runLiveSentimentAnalysis
-from settingHandler import updateSettings
 from settingHandler import *
 
 def processLine(function, specification, parameter):
@@ -9,6 +8,8 @@ def processLine(function, specification, parameter):
             if specification == "sentiment":
                 if len(parameter) == 1:
                     runSentimentAnalysis(parameter[0])
+                elif len(parameter) == 2:
+                    runSentimentAnalysis(parameter[0], parameter[1])
                 else:
                     runSentimentAnalysis(parameter[0], parameter[1])
 
@@ -18,24 +19,38 @@ def processLine(function, specification, parameter):
                 runLiveSentimentAnalysis(parameter[0])
             elif len(parameter) == 2:
                 runLiveSentimentAnalysis(parameter[0], parameter[1])
+            elif len(parameter) == 3:
+                runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2])
             else:
                 runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2])
 
     if function == "config":
         if specification == "datalog":
             settings = loadSettings()
-            if (parameter[0].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh'] or parameter[0].lower() in ['false', '0', 'f', 'n', 'no', 'nop', 'nope', 'mm']):
-                settings["datalog"] = parameter[0].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+            true = ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+            false = ['false', '0', 'f', 'n', 'no', 'nop', 'nope', 'mm']
+            
+            if (parameter[0].lower() in true or parameter[0].lower() in false):
+                settings["datalog"] = parameter[0].lower() in true
             else:
                 print("Please type true or false only to change datalog configuration")
             updateSettings(settings)
-        
+
         if specification == "verbose":
             settings = loadSettings()
-            if (parameter[0].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh'] or parameter[0].lower() in ['false', '0', 'f', 'n', 'no', 'nop', 'nope', 'mm']):
-                settings["verbose"] = parameter[0].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+            if (parameter[0].lower() in true or parameter[0].lower() in false):
+                settings["verbose"] = parameter[0].lower() in true
             else:
                 print("Please type true or false only to change verbose configuration")
             updateSettings(settings)
 
-            
+        if function == "help":
+            if specification == "function":
+                #TO DO
+                #Delete return None after finished here
+                return None
+
+            if specification == "config":
+                #TO DO
+                #Delete return None after finished here
+                return None
