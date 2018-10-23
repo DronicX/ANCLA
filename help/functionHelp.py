7489d44e356e680a8095f4a3016bbf609202620d
Chaliana <chaliana.rolon@upr.edu>
2018-10-23 16:05:02 -0400

    Created a functionHelp file that prints information about functions

--- help/functionHelp.py
+++ help/functionHelp.py
@@ -1,6 +1,10 @@
-#This is our help-function file reader
+#This is our help-function file reader. The user may input 'help-function' wih or without a parameter
+#In doing so, the proper text file with detailed informtion will be printed onto the console
 
 def showFunc(fileName="help-function"):
-    with open("help/" + fileName + ".txt", 'r') as helpFile:
-        for line in helpFile:
-            print(line)
+    try:
+        with open("help/" + fileName + ".txt", 'r') as helpFile:
+            for line in helpFile:
+                print(line)
+    except FileNotFoundError:
+        print("Function doesn't exist.\nTry calling help-function without parameters to see a list of all available functions.")
