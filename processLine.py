d906a5ab4de54fac6cc9984669f607eebdf81b40
DronicX <madeforspams@gmail.com>
2018-10-26 10:07:03 -0400

    Renamed words to reflect new grammar

--- processLine.py
+++ processLine.py
@@ -3,28 +3,28 @@ from analysis.sentimentAnalysis import runSentimentAnalysis
 from analysis.liveSentimentAnalysis import runLiveSentimentAnalysis
 from settingHandler import *
 
-def processLine(function, specification, parameter):
-    if function == "analyze":
+def processLine(action, specification, parameter):
+    if action == "analyze":
             if specification == "sentiment":
                 if len(parameter) == 1:
-                    runSentimentAnalysis(parameter[0])
+                    return runSentimentAnalysis(parameter[0])
                 elif len(parameter) == 2:
-                    runSentimentAnalysis(parameter[0], parameter[1])
+                    return runSentimentAnalysis(parameter[0], parameter[1])
                 else:
-                    runSentimentAnalysis(parameter[0], parameter[1], parameter[2])
+                    return runSentimentAnalysis(parameter[0], parameter[1], parameter[2])
 
-    if function == "live":
+    if action == "live":
         if specification == "sentiment":
             if len(parameter) == 1:
-                runLiveSentimentAnalysis(parameter[0])
+                return runLiveSentimentAnalysis(parameter[0])
             elif len(parameter) == 2:
-                runLiveSentimentAnalysis(parameter[0], parameter[1])
+                return runLiveSentimentAnalysis(parameter[0], parameter[1])
             elif len(parameter) == 3:
-                runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2])
+                return runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2])
             else:
-                runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2], parameter[3])
+                return runLiveSentimentAnalysis(parameter[0], parameter[1], parameter[2], parameter[3])
 
-    if function == "config":
+    if action == "config":
         true = ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
         false = ['false', '0', 'f', 'n', 'no', 'nop', 'nope', 'mm']
 
@@ -45,8 +45,8 @@ def processLine(function, specification, parameter):
                 print("Please type true or false only to change verbose configuration")
             updateSettings(settings)
 
-    if function == "help":
-        if specification == "function":
+    if action == "help":
+        if specification == "action":
             #TO DO
             #Delete return None after finished here
             return None
