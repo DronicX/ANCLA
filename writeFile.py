5ec25d699e78b4a1207b4083f10bca7d1c197bfd
DronicX <madeforspams@gmail.com>
2018-11-06 22:41:45 -0400

    Create folder if it doesn't exist

--- writeFile.py
+++ writeFile.py
@@ -2,10 +2,13 @@ import json
 import time
 from datetime import datetime
 from settingHandler import loadSettings
+import os
 
 def writeToFile(data, folder):
     settings = loadSettings()
     if settings['datalog'] == True:
+        if not os.path.exists('data/' + folder):
+            os.makedirs('data/' + folder)
         name = str(datetime.today().strftime('%d-%m-%Y')) + "_" + str(time.strftime("%Hh-%Mm-%Ss"))+ ".json"
         with open("data/" + folder + "/" + name, 'w') as outfile:
             json.dump(data, outfile, indent=4)
