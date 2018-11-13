b17b434bcbbf8608ea383a93b8bf0fe1c585c365
Chaliana <chaliana.rolon@upr.edu>
2018-11-13 15:24:32 -0400

    Lexical Diversity update, a scatter plot is created with the data

--- analysis/lexicalDiversity.py
+++ analysis/lexicalDiversity.py
@@ -1,4 +1,6 @@
 import tweepy #The Twitter API
+import matplotlib.pyplot as plt #For Graphing the Data
+from settingHandler import loadSettings
 
 CONSUMER_KEY = '5bKnzZoSPFqdyxxqaQ5K3UswP'
 CONSUMER_SECRET = 'L5jQ1a0Ya25aW6GRcMMrHpvrTSoRJGLp7vmA7nC9NeBVpaxIXX'
@@ -11,12 +13,34 @@ api = tweepy.API(auth)
 
 # A function for computing lexical diversity in the text of a set of tweets
 def lexical_diversity(text, count=100):
-    tokens = get_tweets(text,int(count))
-    print(tokens)
-    return 1.0*len(set(tokens))/len(tokens)
+    tweets_text = get_tweets(text,int(count))
+    settings = loadSettings()
+    lexicalValue_list = []
+    number_list = []
+    average = 0
+    number = 1
+
+    for text in tweets_text:
+        words = []
+        for w in text.split():
+            words.append(w)
+        lexicalValue = 1.0*len(set(words))/len(words)
+        lexicalValue_list.append(lexicalValue)
+        average += lexicalValue
+        number_list.append(number)
+        if settings["verbose"] == True:
+            print("Lexical Diversity value from 0 to 1 is " + str(lexicalValue))
+        number += 1
+    if settings["verbose"] == True:
+            print("The average lexical value is " + str(average/len(lexicalValue_list)))
+
+    # Scatter Plot of the data
+    plt.xlabel("Number of Tweets")
+    plt.ylabel("Lexical Diversity")
+    plt.scatter(number_list, lexicalValue_list)
+    plt.show
 
 # Get tweets
 def get_tweets(keyword, tweetNumber):
-    status_text = [tweet.text for tweet in tweepy.Cursor(api.search, keyword,lang="en").items(tweetNumber)]
-    words = [w for t in status_text for w in t.split()]
-    return words
+    statuses_text = [tweet.text for tweet in tweepy.Cursor(api.search, keyword,lang="en").items(tweetNumber)]
+    return statuses_text
