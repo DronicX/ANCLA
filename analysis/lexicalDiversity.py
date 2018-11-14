8ccebfb30a088708edcc2bf547cddcb40a105db8
Chaliana <chaliana.rolon@upr.edu>
2018-11-14 10:52:10 -0400

    Commented code, data is graphable in a scatter plot

--- analysis/lexicalDiversity.py
+++ analysis/lexicalDiversity.py
@@ -13,32 +13,38 @@ api = tweepy.API(auth)
 
 # A function for computing lexical diversity in the text of a set of tweets
 def lexical_diversity(text, count=100):
-    tweets_text = get_tweets(text,int(count))
-    settings = loadSettings()
-    lexicalValue_list = []
-    number_list = []
-    average = 0
-    number = 1
+    try:
+        tweets_text = get_tweets(text,int(count))       # List of the text in all the resulting tweets
+        settings = loadSettings()                       # Instance of the settings
+        lexicalValue_list = []                          # List of all the lexical diversity values calculated
+        number_list = []                                # List of of all the number of tweets
+        sum = 0                                         # Sum of all the lexical diversity values
+        number = 1                                      # Counter of texts
 
-    for text in tweets_text:
-        words = []
-        for w in text.split():
-            words.append(w)
-        lexicalValue = 1.0*len(set(words))/len(words)
-        lexicalValue_list.append(lexicalValue)
-        average += lexicalValue
-        number_list.append(number)
+        # For the text in a list of all the texts
+        for text in tweets_text:
+            words = []
+            # For all the words in the text
+            for w in text.split():
+                words.append(w)
+            lexicalValue = 1.0*len(set(words))/len(words)      # Calculate the lexical diversity
+            lexicalValue_list.append(lexicalValue)
+            sum += lexicalValue
+            number_list.append(number)
+            if settings["verbose"] == True:
+                print("Lexical Diversity value from 0 to 1 is " + str(lexicalValue))
+            number += 1
         if settings["verbose"] == True:
-            print("Lexical Diversity value from 0 to 1 is " + str(lexicalValue))
-        number += 1
-    if settings["verbose"] == True:
-            print("The average lexical value is " + str(average/len(lexicalValue_list)))
+                print("The average lexical value is " + str(sum/len(lexicalValue_list)))
 
-    # Scatter Plot of the data
-    plt.xlabel("Number of Tweets")
-    plt.ylabel("Lexical Diversity")
-    plt.scatter(number_list, lexicalValue_list)
-    plt.show
+        # Scatter Plot of the data
+        plt.xlabel("Number of Tweets")
+        plt.ylabel("Lexical Diversity")
+        plt.scatter(number_list, lexicalValue_list)
+        plt.show
+        
+     except tweepy.TweepError as e:
+            print(e.reason)
 
 # Get tweets
 def get_tweets(keyword, tweetNumber):
