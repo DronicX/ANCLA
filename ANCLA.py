ceaca79c3ceae376fb1bafe3497e20496d92cea5
DronicX <madeforspams@gmail.com>
2018-11-18 21:53:43 -0400

    Added ability to see ANCLA general help

--- ANCLA.py
+++ ANCLA.py
@@ -15,20 +15,25 @@ def main(argv):
         user_input = input("-> ")
         if user_input.lower() == "q" or user_input.lower() == "quit" or user_input.lower() == "exit":   
             break
-
-        #Boilerplate code for antlr4
-        i_stream = InputStream(user_input)
-        lexer = ANCLALexer(i_stream)
-        stream = CommonTokenStream(lexer)
-        parser = ANCLAParser(stream)
-        parser.buildParseTrees = True
-        #Here we parse our input as a line
-        tree = parser.exp()
         
-        if parser.getNumberOfSyntaxErrors() < 1:
-            ANCLA = CustomANCLAListener()
-            walker = ParseTreeWalker()
-            walker.walk(ANCLA, tree)
+        if user_input.lower() == "help" or user_input.lower() == "manual" or user_input.lower() == "man":
+            with open("help/manual.txt", 'r') as helpFile:
+                for line in helpFile:
+                    print(line)
+        else:
+            #Boilerplate code for antlr4
+            i_stream = InputStream(user_input)
+            lexer = ANCLALexer(i_stream)
+            stream = CommonTokenStream(lexer)
+            parser = ANCLAParser(stream)
+            parser.buildParseTrees = True
+            #Here we parse our input as a line
+            tree = parser.exp()
+            
+            if parser.getNumberOfSyntaxErrors() < 1:
+                ANCLA = CustomANCLAListener()
+                walker = ParseTreeWalker()
+                walker.walk(ANCLA, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
