47f06b34e686f0f6d46ee5a86d2a665be3e25142
Chaliana <chaliana.rolon@upr.edu>
2018-11-27 18:27:30 -0400

    Editing print and average function help files

--- analysis/lexicalDiversity.py
+++ analysis/lexicalDiversity.py
@@ -20,7 +20,7 @@ def lexical_diversity(text, count=100):
         settings = loadSettings()                       # Instance of the settings
         lexicalValue_list = []                          # List of all the lexical diversity values calculated
         number_list = []                                # List of of all the number of tweets
-        sum = 0                                         # Sum of all the lexical diversity values
+        suma = 0                                        # Sum of all the lexical diversity values
         number = 1                                      # Counter of texts
 
         # For the text in a list of all the texts
@@ -31,13 +31,13 @@ def lexical_diversity(text, count=100):
                 words.append(w)
             lexicalValue = 1.0*len(set(words))/len(words)      # Calculate the lexical diversity
             lexicalValue_list.append(lexicalValue)
-            sum += lexicalValue
+            suma += lexicalValue
             number_list.append(number)
             if settings["verbose"] == True:
                 print("Lexical Diversity value from 0 to 1 is " + str(lexicalValue))
             number += 1
         if settings["verbose"] == True:
-                print("The average lexical value is " + str(sum/len(lexicalValue_list)))
+                print("The average lexical value is " + str(suma/len(lexicalValue_list)))
 
         # Scatter Plot of the data
         plt.xlabel("Number of Tweets")
